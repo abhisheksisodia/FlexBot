01e00e50b466737252a5af3f8728a7204ac20e68
Abhishek Sisodia <abhishek.sisodia@td.com>
2017-03-26 22:53:09 -0400

    Added LUIS integration and some simple responses

--- FlexBot/FlexBot/Controllers/MessagesController.cs
+++ FlexBot/FlexBot/Controllers/MessagesController.cs
@@ -19,14 +19,45 @@ namespace FlexBot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message && activity.Text.Contains("@flexbot"))
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            if (activity.Type == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                string replyString;
+                LuisResponse StLUIS = await GetEntityFromLUIS(activity.Text);
+                if (StLUIS.intents.Count() > 0)
+                {
+                    switch (StLUIS.intents[0].intent)
+                    {
+                        case "PeopleList":
+                            replyString = "Here is a list of people: Abhishek, Filip, Daniel, Anthony, Camilo";
+                            break;
+                        case "Intro":
+                            replyString = "I am FlexBot. I can help you find people within your organization based on skill set, knowledge level and more";
+                            break;
+                        case "None":
+                            replyString = "Please ask something meaningful. For instance, you can ask me about to give a list of people who are skilled in a particular technology";
+                            break;
+                        default:
+                            replyString = "Sorry, I am unable to understand...";
+                            break;
+                    }
+                }
+                else
+                {
+                    replyString = "Sorry, I am not getting you...";
+                }
 
+                Activity reply = activity.CreateReply(replyString);
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"Hi, I am FlexBot. I can help you find people based on skill set, knowledge level, location and more!");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                string replyMessage = string.Empty;
+                replyMessage += $"Hi there\n\n";
+                replyMessage += $"I am FlexBot. Designed to help you find people within your company.  \n";
+                replyMessage += $"I can help you find people based on skill set, knowledge level, location and more!  \n";
+                Activity reply = activity.CreateReply(replyMessage);
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
@@ -64,6 +95,24 @@ namespace FlexBot
             }
 
             return null;
+        }
+
+        private static async Task<LuisResponse> GetEntityFromLUIS(string Query)
+        {
+            Query = Uri.EscapeDataString(Query);
+            LuisResponse Data = new LuisResponse();
+            using (HttpClient client = new HttpClient())
+            {
+                string RequestURI = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/eb1f40b0-c8da-4f20-9264-c8d175117dea?subscription-key=9bee6378df734716b7e2c3f346b30219&timezoneOffset=0.0&verbose=true&q=" + Query;
+                HttpResponseMessage msg = await client.GetAsync(RequestURI);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var JsonDataResponse = await msg.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<LuisResponse>(JsonDataResponse);
+                }
+            }
+            return Data;
         }
     }
 }

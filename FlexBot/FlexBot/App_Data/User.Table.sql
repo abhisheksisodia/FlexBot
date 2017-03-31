0e261e205001461e47393cb9934a6ce05b7505c8
Abhishek Sisodia <abhishek.sisodia@td.com>
2017-03-31 11:52:32 -0400

    Added sample database

---
+++ FlexBot/FlexBot/App_Data/User.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [Location] INT NULL, 
+    [HiringDate] DATE NULL, 
+    [ManagerId] INT NULL, 
+    [Email] VARCHAR(50) NULL, 
+    [Phone] VARCHAR(50) NULL 
+)

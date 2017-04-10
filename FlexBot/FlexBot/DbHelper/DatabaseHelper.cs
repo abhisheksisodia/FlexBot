0c29ef18af24534771ce981a94d02c287ee66427
Daniel <djeanbaptiste@infusion.com>
2017-04-10 14:12:07 -0400

    - Add new Stored procedure - Update DatabaseHelper

--- FlexBot/FlexBot/DbHelper/DatabaseHelper.cs
+++ FlexBot/FlexBot/DbHelper/DatabaseHelper.cs
@@ -6,124 +6,170 @@ namespace FlexBot.DbHelper
 {
     public class DatabaseHelper
     {
-        private SqlConnection connection;
+        //private SqlConnection connection;
+        private string connectionString;
         public DatabaseHelper() {
             //string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\baptid3\\Source\\Repos\\FlexBot\\FlexBot\\FlexBot\\App_Data\\SkillsDatabase.mdf;Integrated Security=True";
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionStringName"].ConnectionString;
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionStringName"].ConnectionString;
 
-            connection = new SqlConnection(connectionString);
+            //connection = new SqlConnection(connectionString);
         }
 
         public List<UserSkillsView> GetUserByLocationAndSkill(string location, string skills) {
-            
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand();
+                SqlDataReader dataReader;
 
-            command.CommandText = "dbo.GetUsersBySkillAndLocation";
-            command.Parameters["skill"].Value = skills;
-            command.Parameters["location"].Value = location;
+                SqlParameter skillParam = new SqlParameter();
+                skillParam.ParameterName = "@skill";
+                skillParam.Value = skills;
+                command.Parameters.Add(skillParam);
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
+                SqlParameter locationParam = new SqlParameter();
+                locationParam.ParameterName = "@location";
+                locationParam.Value = location;
+                command.Parameters.Add(locationParam);
 
-            connection.Open();
-            dataReader = command.ExecuteReader();
-           
-            // Parse dateReader
-            while (dataReader.Read()) { 
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = connection;
+                List<UserSkillsView> result = new List<UserSkillsView>();
 
-                result.Add(readRow(dataReader));
+                connection.Open();
+                dataReader = command.ExecuteReader();
+
+                // Parse dateReader
+                while (dataReader.Read())
+                {
+
+                    result.Add(readRow(dataReader));
+                }
+
+                //dataReader.Close();
+                //connection.Close();
+
+                return result;
             }
-
-            dataReader.Close();
-            connection.Close();
-
-            return result;
         }
 
         public List<UserSkillsView> GetUserByLocationAndProficiency(string location, string proficiency) {
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
-
-            command.CommandText = "dbo.GetUsersBySkillAndProficiency";
-            command.Parameters["proficiency"].Value = proficiency;
-            command.Parameters["location"].Value = location;
-
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
-
-            connection.Open();
-            dataReader = command.ExecuteReader();
-
-            // Parse dateReader
-            while (dataReader.Read())
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                result.Add(readRow(dataReader));
+                SqlCommand command = new SqlCommand();
+                SqlDataReader dataReader;
+
+                SqlParameter proficiencyParam = new SqlParameter();
+                proficiencyParam.ParameterName = "@proficiency";
+                proficiencyParam.Value = proficiency;
+                command.Parameters.Add(proficiencyParam);
+
+                SqlParameter locationParam = new SqlParameter();
+                locationParam.ParameterName = "@location";
+                locationParam.Value = location;
+                command.Parameters.Add(locationParam);
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = connection;
+                List<UserSkillsView> result = new List<UserSkillsView>();
+
+                connection.Open();
+                dataReader = command.ExecuteReader();
+
+                // Parse dateReader
+                while (dataReader.Read())
+                {
+                    result.Add(readRow(dataReader));
+                }
+
+                //dataReader.Close();
+                //connection.Close();
+
+                return result;
             }
-
-            dataReader.Close();
-            connection.Close();
-
-            return result;
         }
 
         public List<UserSkillsView> GetUserBySkillAndProficiency(string skill, string proficiency)
         {
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
-
-            command.CommandText = "dbo.GetUsersBySkillAndProficiency";
-            command.Parameters["proficiency"].Value = proficiency;
-            command.Parameters["skill"].Value = skill;
-
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
-
-            connection.Open();
-            dataReader = command.ExecuteReader();
-
-            // Parse dateReader
-            while (dataReader.Read())
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                result.Add(readRow(dataReader));
+                SqlCommand command = new SqlCommand();
+                SqlDataReader dataReader;
+
+                SqlParameter proficiencyParam = new SqlParameter();
+                proficiencyParam.ParameterName = "@proficiency";
+                proficiencyParam.Value = proficiency;
+                command.Parameters.Add(proficiencyParam);
+
+                SqlParameter skillParam = new SqlParameter();
+                skillParam.ParameterName = "@skill";
+                skillParam.Value = skill;
+                command.Parameters.Add(skillParam);
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = connection;
+                List<UserSkillsView> result = new List<UserSkillsView>();
+
+                connection.Open();
+                dataReader = command.ExecuteReader();
+
+                // Parse dateReader
+                while (dataReader.Read())
+                {
+                    result.Add(readRow(dataReader));
+                }
+
+                //dataReader.Close();
+                //connection.Close();
+
+                return result;
             }
-
-            dataReader.Close();
-            connection.Close();
-
-            return result;
         }
 
         public List<UserSkillsView> GetUserBySkillProficiencyAndLocation(string skill, string proficiency, string location)
         {
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
-
-            command.CommandText = "dbo.GetUsersBySkillLocationProficiency";
-            command.Parameters["proficiency"].Value = proficiency;
-            command.Parameters["skill"].Value = skill;
-            command.Parameters["location"].Value = location;
-
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
-
-            connection.Open();
-            dataReader = command.ExecuteReader();
-
-            // Parse dateReader
-            while (dataReader.Read())
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                result.Add(readRow(dataReader));
+                SqlCommand command = new SqlCommand();
+                SqlDataReader dataReader;
+
+                command.CommandText = "dbo.GetUsersBySkillLocationProficiency";
+
+                SqlParameter proficiencyParam = new SqlParameter();
+                proficiencyParam.ParameterName = "@proficiency";
+                proficiencyParam.Value = proficiency;
+                command.Parameters.Add(proficiencyParam);
+
+                SqlParameter skillParam = new SqlParameter();
+                skillParam.ParameterName = "@skill";
+                skillParam.Value = skill;
+                command.Parameters.Add(skillParam);
+
+                SqlParameter locationParam = new SqlParameter();
+                locationParam.ParameterName = "@location";
+                locationParam.Value = location;
+                command.Parameters.Add(locationParam);
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = connection;
+                List<UserSkillsView> result = new List<UserSkillsView>();
+
+                connection.Open();
+                dataReader = command.ExecuteReader();
+
+                // Parse dateReader
+                while (dataReader.Read())
+                {
+                    result.Add(readRow(dataReader));
+                }
+
+                return result;
             }
+                
 
-            dataReader.Close();
-            connection.Close();
+            //dataReader.Close();
+            //connection.Close();
 
-            return result;
+            
         }
 
 

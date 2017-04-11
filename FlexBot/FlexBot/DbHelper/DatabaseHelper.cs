a37f8632677173f556faec37a426206042148bb6
Abhishek Sisodia <abhishek.sisodia@td.com>
2017-04-11 11:27:54 -0400

    Temp comment out db stuff to get build working

--- FlexBot/FlexBot/DbHelper/DatabaseHelper.cs
+++ FlexBot/FlexBot/DbHelper/DatabaseHelper.cs
@@ -6,141 +6,141 @@ namespace FlexBot.DbHelper
 {
     public class DatabaseHelper
     {
-        private SqlConnection connection;
-        public DatabaseHelper() {
-            //string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\baptid3\\Source\\Repos\\FlexBot\\FlexBot\\FlexBot\\App_Data\\SkillsDatabase.mdf;Integrated Security=True";
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionStringName"].ConnectionString;
+        //private SqlConnection connection;
+        //public DatabaseHelper() {
+        //    //string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\baptid3\\Source\\Repos\\FlexBot\\FlexBot\\FlexBot\\App_Data\\SkillsDatabase.mdf;Integrated Security=True";
+        //    string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionStringName"].ConnectionString;
 
-            connection = new SqlConnection(connectionString);
-        }
+        //    connection = new SqlConnection(connectionString);
+        //}
 
-        public List<UserSkillsView> GetUserByLocationAndSkill(string location, string skills) {
+        //public List<UserSkillsView> GetUserByLocationAndSkill(string location, string skills) {
             
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
+        //    SqlCommand command = new SqlCommand();
+        //    SqlDataReader dataReader;
 
-            command.CommandText = "dbo.GetUsersBySkillAndLocation";
-            command.Parameters["skill"].Value = skills;
-            command.Parameters["location"].Value = location;
+        //    command.CommandText = "dbo.GetUsersBySkillAndLocation";
+        //    command.Parameters["skill"].Value = skills;
+        //    command.Parameters["location"].Value = location;
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
+        //    command.CommandType = CommandType.StoredProcedure;
+        //    command.Connection = connection;
+        //    List<UserSkillsView> result = new List<UserSkillsView>();
 
-            connection.Open();
-            dataReader = command.ExecuteReader();
+        //    connection.Open();
+        //    dataReader = command.ExecuteReader();
            
-            // Parse dateReader
-            while (dataReader.Read()) { 
+        //    // Parse dateReader
+        //    while (dataReader.Read()) { 
 
-                result.Add(readRow(dataReader));
-            }
+        //        result.Add(readRow(dataReader));
+        //    }
 
-            dataReader.Close();
-            connection.Close();
+        //    dataReader.Close();
+        //    connection.Close();
 
-            return result;
-        }
+        //    return result;
+        //}
 
-        public List<UserSkillsView> GetUserByLocationAndProficiency(string location, string proficiency) {
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
+        //public List<UserSkillsView> GetUserByLocationAndProficiency(string location, string proficiency) {
+        //    SqlCommand command = new SqlCommand();
+        //    SqlDataReader dataReader;
 
-            command.CommandText = "dbo.GetUsersBySkillAndProficiency";
-            command.Parameters["proficiency"].Value = proficiency;
-            command.Parameters["location"].Value = location;
+        //    command.CommandText = "dbo.GetUsersBySkillAndProficiency";
+        //    command.Parameters["proficiency"].Value = proficiency;
+        //    command.Parameters["location"].Value = location;
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
+        //    command.CommandType = CommandType.StoredProcedure;
+        //    command.Connection = connection;
+        //    List<UserSkillsView> result = new List<UserSkillsView>();
 
-            connection.Open();
-            dataReader = command.ExecuteReader();
+        //    connection.Open();
+        //    dataReader = command.ExecuteReader();
 
-            // Parse dateReader
-            while (dataReader.Read())
-            {
-                result.Add(readRow(dataReader));
-            }
+        //    // Parse dateReader
+        //    while (dataReader.Read())
+        //    {
+        //        result.Add(readRow(dataReader));
+        //    }
 
-            dataReader.Close();
-            connection.Close();
+        //    dataReader.Close();
+        //    connection.Close();
 
-            return result;
-        }
+        //    return result;
+        //}
 
-        public List<UserSkillsView> GetUserBySkillAndProficiency(string skill, string proficiency)
-        {
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
+        //public List<UserSkillsView> GetUserBySkillAndProficiency(string skill, string proficiency)
+        //{
+        //    SqlCommand command = new SqlCommand();
+        //    SqlDataReader dataReader;
 
-            command.CommandText = "dbo.GetUsersBySkillAndProficiency";
-            command.Parameters["proficiency"].Value = proficiency;
-            command.Parameters["skill"].Value = skill;
+        //    command.CommandText = "dbo.GetUsersBySkillAndProficiency";
+        //    command.Parameters["proficiency"].Value = proficiency;
+        //    command.Parameters["skill"].Value = skill;
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
+        //    command.CommandType = CommandType.StoredProcedure;
+        //    command.Connection = connection;
+        //    List<UserSkillsView> result = new List<UserSkillsView>();
 
-            connection.Open();
-            dataReader = command.ExecuteReader();
+        //    connection.Open();
+        //    dataReader = command.ExecuteReader();
 
-            // Parse dateReader
-            while (dataReader.Read())
-            {
-                result.Add(readRow(dataReader));
-            }
+        //    // Parse dateReader
+        //    while (dataReader.Read())
+        //    {
+        //        result.Add(readRow(dataReader));
+        //    }
 
-            dataReader.Close();
-            connection.Close();
+        //    dataReader.Close();
+        //    connection.Close();
 
-            return result;
-        }
+        //    return result;
+        //}
 
-        public List<UserSkillsView> GetUserBySkillProficiencyAndLocation(string skill, string proficiency, string location)
-        {
-            SqlCommand command = new SqlCommand();
-            SqlDataReader dataReader;
+        //public List<UserSkillsView> GetUserBySkillProficiencyAndLocation(string skill, string proficiency, string location)
+        //{
+        //    SqlCommand command = new SqlCommand();
+        //    SqlDataReader dataReader;
 
-            command.CommandText = "dbo.GetUsersBySkillLocationProficiency";
-            command.Parameters["proficiency"].Value = proficiency;
-            command.Parameters["skill"].Value = skill;
-            command.Parameters["location"].Value = location;
+        //    command.CommandText = "dbo.GetUsersBySkillLocationProficiency";
+        //    command.Parameters["proficiency"].Value = proficiency;
+        //    command.Parameters["skill"].Value = skill;
+        //    command.Parameters["location"].Value = location;
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Connection = connection;
-            List<UserSkillsView> result = new List<UserSkillsView>();
+        //    command.CommandType = CommandType.StoredProcedure;
+        //    command.Connection = connection;
+        //    List<UserSkillsView> result = new List<UserSkillsView>();
 
-            connection.Open();
-            dataReader = command.ExecuteReader();
+        //    connection.Open();
+        //    dataReader = command.ExecuteReader();
 
-            // Parse dateReader
-            while (dataReader.Read())
-            {
-                result.Add(readRow(dataReader));
-            }
+        //    // Parse dateReader
+        //    while (dataReader.Read())
+        //    {
+        //        result.Add(readRow(dataReader));
+        //    }
 
-            dataReader.Close();
-            connection.Close();
+        //    dataReader.Close();
+        //    connection.Close();
 
-            return result;
-        }
+        //    return result;
+        //}
 
 
-        private UserSkillsView readRow(IDataRecord rowData) {
-            UserSkillsViewBuilder builder = new UserSkillsViewBuilder();
+        //private UserSkillsView readRow(IDataRecord rowData) {
+        //    UserSkillsViewBuilder builder = new UserSkillsViewBuilder();
 
-            builder.Id(rowData.GetInt32(0));
-            builder.FirstName(rowData.GetString(1));
-            builder.LastName(rowData.GetString(2));
-            builder.HiringDate(rowData.GetDateTime(3));
-            builder.Email(rowData.GetString(4));
-            builder.PhoneNumber(rowData.GetString(5));
-            builder.Skill(rowData.GetString(6));
-            builder.Level(rowData.GetString(7));
-            builder.Location(rowData.GetString(8));
+        //    builder.Id(rowData.GetInt32(0));
+        //    builder.FirstName(rowData.GetString(1));
+        //    builder.LastName(rowData.GetString(2));
+        //    builder.HiringDate(rowData.GetDateTime(3));
+        //    builder.Email(rowData.GetString(4));
+        //    builder.PhoneNumber(rowData.GetString(5));
+        //    builder.Skill(rowData.GetString(6));
+        //    builder.Level(rowData.GetString(7));
+        //    builder.Location(rowData.GetString(8));
 
-            return builder.Build();
-        }
+        //    return builder.Build();
+        //}
     }
 }
--- FlexBot/FlexBot/DbHelper/UserSkillsViewBuilder.cs
+++ FlexBot/FlexBot/DbHelper/UserSkillsViewBuilder.cs
@@ -7,63 +7,63 @@ namespace FlexBot.DbHelper
 {
     public class UserSkillsViewBuilder
     {
-        private UserSkillsView userSkillsView;
+        //private UserSkillsView userSkillsView;
 
-        public UserSkillsViewBuilder() {
-            userSkillsView = new UserSkillsView();
-        }
+        //public UserSkillsViewBuilder() {
+        //    userSkillsView = new UserSkillsView();
+        //}
 
-        public UserSkillsViewBuilder Id(int id) {
-            userSkillsView.Id = id;
-            return this;
-        }
+        //public UserSkillsViewBuilder Id(int id) {
+        //    userSkillsView.Id = id;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder FirstName(string firstName) {
-            userSkillsView.FirstName = firstName;
-            return this;
-        }
+        //public UserSkillsViewBuilder FirstName(string firstName) {
+        //    userSkillsView.FirstName = firstName;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder LastName(string lastName) {
-            userSkillsView.LastName = lastName;
-            return this;
-        }
+        //public UserSkillsViewBuilder LastName(string lastName) {
+        //    userSkillsView.LastName = lastName;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder HiringDate(DateTime hiringDate) {
-            userSkillsView.HiringDate = hiringDate;
-            return this;
-        }
+        //public UserSkillsViewBuilder HiringDate(DateTime hiringDate) {
+        //    userSkillsView.HiringDate = hiringDate;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder Skill(string skill) {
-            userSkillsView.Skill = skill;
-            return this;
-        }
+        //public UserSkillsViewBuilder Skill(string skill) {
+        //    userSkillsView.Skill = skill;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder PhoneNumber(String phoneNumber) {
-            userSkillsView.Phone = phoneNumber;
-            return this;
-        }
+        //public UserSkillsViewBuilder PhoneNumber(String phoneNumber) {
+        //    userSkillsView.Phone = phoneNumber;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder Level(String level) {
-            userSkillsView.Level = level;
-            return this;
-        }
+        //public UserSkillsViewBuilder Level(String level) {
+        //    userSkillsView.Level = level;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder Email(string email) {
-            userSkillsView.Email = email;
-            return this;
-        }
+        //public UserSkillsViewBuilder Email(string email) {
+        //    userSkillsView.Email = email;
+        //    return this;
+        //}
 
-        public UserSkillsViewBuilder Location(string location) {
-            userSkillsView.Location = location;
-            return this;
-        }
+        //public UserSkillsViewBuilder Location(string location) {
+        //    userSkillsView.Location = location;
+        //    return this;
+        //}
 
-        public UserSkillsView Build() {
-            return userSkillsView;
-        }
+        //public UserSkillsView Build() {
+        //    return userSkillsView;
+        //}
 
-        public void Clear() {
-            userSkillsView = new UserSkillsView();
-        }
+        //public void Clear() {
+        //    userSkillsView = new UserSkillsView();
+        //}
     }
 }

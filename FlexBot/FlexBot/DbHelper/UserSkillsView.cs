d5ffbe14686148abb99baf8755e08c72e4a12ab2
Daniel <djeanbaptiste@infusion.com>
2017-04-12 00:13:21 -0400

    Connect to Azure Database

--- FlexBot/FlexBot/DbHelper/UserSkillsView.cs
+++ FlexBot/FlexBot/DbHelper/UserSkillsView.cs
@@ -4,25 +4,14 @@ namespace FlexBot.DbHelper
 {
     public class UserSkillsView
     {
-        private int id;
-        private string firstName;
-        private string lastName;
-        private DateTime hiringDate;
-        private string email;
-        private string phone;
-        private string skill;
-        private string level;
-        private string location;
-
-
-        public int Id { get => id; set => id = value; }
-        public string FirstName { get => firstName; set => firstName = value; }
-        public string LastName { get => lastName; set => lastName = value; }
-        public DateTime HiringDate { get => hiringDate; set => hiringDate = value; }
-        public string Email { get => email; set => email = value; }
-        public string Phone { get => phone; set => phone = value; }
-        public string Skill { get => skill; set => skill = value; }
-        public string Level { get => level; set => level = value; }
-        public string Location { get => location; set => location = value; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HiringDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Skill { get; set; }
+        public string Level { get; set; }
+        public string Location { get; set; }
     }
 }
